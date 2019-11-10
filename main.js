bdb271311f307550b3ccc3022c80737db0fb9dfc
wisara88 <srandall845@gmail.com>
2019-11-09 17:38:44 -0800

    comments added

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var allTags = document.getElementsByTagName('a');
 document.addEventListener("keydown", function(event) {
   // console.log(event.which);
   if (event.which == 70) {
-    // console.log("you typed f");
+    console.log("you typed f"); // not console logging with anchor tag. Loops and crashes Chrome
     for (var i = 0; i < allTags.length; i++) {
       var newAtt = allTags[i];
       var tagText = newAtt.textContent;
       var tagHref = newAtt.href;
       // console.log("before anchor");
-      var shortCut = document.createElement('a');
+      var shortCut = document.createElement('a'); // not creating a tag
       // shortCut.textContent = tagText;
       // console.log("after anchor")
       shortCut.href = tagHref;
